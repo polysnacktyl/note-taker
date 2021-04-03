4b46b85dc3082d2d793aabfcc81328c966887468
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-04-03 12:25:29 -0400

    got index.js linked up, working on getting fetch api/notes to connect with db.json

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,24 +1,32 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
-//process.env.PORT ||
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('/public/'));
+app.use(express.static(path.join(__dirname, 'assets/js/')));
 
 app.get("/", (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/", (req, res, next) => {
+app.use('indexjs', function (req, res) {
+    res.sendFile('index.js');
+});
+
+app.get("/index", (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-   });
+});
 
 app.get("/notes", (req, res, next) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
+app.get("/api/notes", (req, res, next) => {
+    res.json(notes);
+        console.log(notes);
+    });
 
-app.listen(port, () => console.log(`App listening on port: ${port}`));
+    app.listen(port, () => console.log(`App listening on port: ${port}`));
