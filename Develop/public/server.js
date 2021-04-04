fa9013731253bc46e5f56bdce01a97d5460ad66b
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-04-04 19:49:23 -0400

    hooked up index.js and styles.cs, got add and delete note functions functioning. next: deploy to heroku, hope it plays nice.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,32 +1,74 @@
-const express = require("express");
+const express = require('express')
 const app = express();
-const port = process.env.PORT || 3000;
 const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('/public/'));
+
 app.use(express.static(path.join(__dirname, 'assets/js/')));
-
-app.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.use('indexjs', function (req, res) {
+app.use('indexjs', (req, res, next) => {
     res.sendFile('index.js');
 });
 
-app.get("/index", (req, res, next) => {
+app.use(express.static(path.join(__dirname, 'assets/css/')));
+app.use('css', (req, res) => {
+    res.sendFile('styles.css');
+});
+
+app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/notes", (req, res, next) => {
+app.get('/index', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-app.get("/api/notes", (req, res, next) => {
-    res.json(notes);
-        console.log(notes);
+app.get('/api/notes', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'db.json'));
+});
+
+app.get('/api/notes/:id', (req, res, next) => {
+    let allTheNotes = JSON.parse(fs.readFileSync('db.json'));
+    res.json(allTheNotes[Number(req.params.id)]);
+});
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/api/notes', (req, res, next) => {
+    let allTheNotes = JSON.parse(fs.readFileSync('db.json'));
+    let newNote = req.body;
+    let noteID = (allTheNotes.length).toString();
+    newNote.id = noteID;
+    allTheNotes.push(newNote);
+
+    fs.writeFileSync('db.json', JSON.stringify(allTheNotes));
+    res.json(allTheNotes);
+});
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    let allTheNotes = JSON.parse(fs.readFileSync('db.json'));
+    let noteID = req.params.id;
+    let newID = 0;
+    allTheNotes = allTheNotes.filter(deleteThisNote => {
+        return deleteThisNote.id != noteID;
     });
 
-    app.listen(port, () => console.log(`App listening on port: ${port}`));
+    for (deleteThisNote of allTheNotes) {
+        deleteThisNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync('db.json', JSON.stringify(allTheNotes));
+    res.json(allTheNotes);
+});
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
